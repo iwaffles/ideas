e255a15b75c817ff0331cbe7fa73774771e4683f
Matt Gardner <iwaffles@gmail.com>
2012-05-12 12:48:31 -0400

    Set default route.

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Ideas::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'ideas#index'
 
   # See how all your routes lay out with "rake routes"
 
